7bf1eee47b5495fccae547ddba466f6fcff1a57f
yura-prots <yuraprots@gmail.com>
2022-07-18 11:12:19 +0300

    add light - darck mode

--- weather-front/src/App.vue
+++ weather-front/src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
+    <v-content>
+      <v-switch
+        @click="toggleTheme"
+        v-model="model"
+        :label="`${this.theme.global.name.value.toString()}`"
+      ></v-switch>
+      <router-view />
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import { useTheme } from "vuetify";
 
 export default {
-  name: 'App',
+  name: "App",
 
-  data: () => ({
-    //
-  }),
-}
+  data() {
+    return {
+      model: true,
+    };
+  },
+
+  setup() {
+    const theme = useTheme();
+    return {
+      theme,
+      toggleTheme: () =>
+        (theme.global.name.value = theme.global.current.value.dark
+          ? "light"
+          : "dark"),
+    };
+  },
+};
 </script>
--- weather-front/src/pages/Home/index.vue
+++ weather-front/src/pages/Home/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Test weather project</div>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
 </template>
 
 <script>
